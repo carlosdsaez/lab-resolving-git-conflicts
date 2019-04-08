b75ceb123fcb486825b91aa00bd00dc3841918c3
Carlos Dueñas <carlosdss84@gmail.com>
2019-04-08 16:10:29 +0200

    First commint on hotfix

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 #This is the very first new file of the camp
 for i in range(1, 10):
     print('Hola ' * (i * 2 ))
+
+for i in range(1, 15):
+    print('Adios ' * (i * 3 ))
+    
 print('Fin de la cita')
