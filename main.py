ab5cc2b44105c50a4781775acd837a97f56276d2
Carlos Dueñas <carlosdss84@gmail.com>
2019-04-08 16:12:49 +0200

    Second commint on hotfix

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 #This is the very first new file of the camp
 for i in range(1, 10):
     print('Hola ' * (i * 2 ))
+
+a = [1, 2, 3, 4]
+
+for number in a:
+    print("I see ", number, " tree/s")
+
 print('Fin de la cita')
+
